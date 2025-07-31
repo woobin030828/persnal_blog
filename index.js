7e7e7246167166cc3e32b7cd3f81bc305926360e
CWB\fpqk3 <wbu3us2@gmail.com>
2025-07-31 15:22:44 +0900

    인덱스 만들기 등등

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('public'));
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/home/index.html');
+});
+app.get('/dev', (req, res) => {
+  res.sendFile(__dirname + '/public/dev/index.html');
+});
+app.get('/game', (req, res) => {
+  res.sendFile(__dirname + '/public/game/index.html');
+});
+app.get('/daily', (req, res) => {
+  res.sendFile(__dirname + '/public/daily/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// 이건 테스트용입니다^^
